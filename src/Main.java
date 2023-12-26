d361dbae36cd3bd85c1082f3b3e9aaed64df854d
Владимир <vkuznecov1504@mail.ru>
2023-12-26 12:14:40 +0300

    Инициализировали переменную firstBoxerWeight со значением 78.2 Инициализировали переменную secondBoxerWeight со значением 82.7 Подсчитали общий вес бойцов Нашли разницу в весе бойцов

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,21 @@ public class Main {
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var boxerWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес боксеров " + boxerWeight);
+        var differenceWeight = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница в весе боксеров " + differenceWeight);
     }
 }
